f38825e86cffc9a160035fef349b453ddef4eadf
StarrZhong <dark781024@hotmail.com>
2022-04-21 07:11:11 +0800

    add dist

---
+++ dist/js/sourceData.js
@@ -0,0 +1,37 @@
+var sourceData = [
+  {
+    "title": "主色系",
+    "colorList":[
+      {
+        "name":"primary",
+        "code":"#f80",
+        "value":"#f80"
+      },
+      {
+        "name":"primary2",
+        "code":"#f60",
+        "value":"#f60"
+      }
+    ]
+  },
+  {
+    "title": "灰階",
+    "colorList":[
+      {
+        "name":"gray-100",
+        "code":"#f2f2f2",
+        "value":"#f2f2f2"
+      },
+      {
+        "name":"gray-200",
+        "code":"#ddd",
+        "value":"#ddd"
+      },
+      {
+        "name":"gray-300",
+        "code":"#ccc",
+        "value":"#ccc"
+      }
+    ]
+  }
+]
